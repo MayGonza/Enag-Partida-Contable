b4381c0a1287edbdde928d6a5a2aa5723f11e929
MayGonza <101020558+MayGonza@users.noreply.github.com>
2026-06-10 10:40:37 -0600

    pdf

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 750,
+    width: 1200, // Mantener un ancho predeterminado razonable
+    height: 750, // Mantener una altura predeterminada razonable
+    minWidth: 350, // Permitir que la ventana se reduzca a dimensiones de móvil
+    minHeight: 600, // Establecer una altura mínima para la usabilidad
     resizable: true
   });
 
